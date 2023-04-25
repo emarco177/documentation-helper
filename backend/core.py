0dcefb7f1a634e53717b3007e2418172f49056d4
Eden Marco <emarco177@gmail.com>
2023-04-25 10:56:01 +0300

    first commit

---
+++ backend/core.py
@@ -0,0 +1,34 @@
+import os
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
+from langchain.vectorstores import Pinecone
+import pinecone
+
+
+pinecone.init(
+    api_key=os.environ["PINECONE_API_KEY"],
+    environment=os.environ["PINECONE_ENVIRONMENT_REGION"],
+)
+
+INDEX_NAME = "medium-blogs-embeddings-index"
+
+
+def run_llm(query: str):
+    embeddings = OpenAIEmbeddings(openai_api_key=os.environ["OPENAI_API_KEY"])
+    docsearch = Pinecone.from_existing_index(
+        embedding=embeddings,
+        index_name=INDEX_NAME,
+    )
+    chat = ChatOpenAI(
+        verbose=True,
+        temperature=0,
+    )
+
+    qa = RetrievalQA.from_chain_type(
+        llm=chat,
+        chain_type="stuff",
+        retriever=docsearch.as_retriever(),
+        return_source_documents=True,
+    )
+    return qa({"query": query})
