8064a8c160ed4f3d6a3ca714e581886a23db819e
Eden Marco <emarco177@gmail.com>
2023-04-26 22:52:34 +0300

    improved UI and added memory

--- backend/core.py
+++ backend/core.py
@@ -1,7 +1,9 @@
 import os
+from typing import Any, Dict, List
+
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import RetrievalQA
+from langchain.chains import ConversationalRetrievalChain
 from langchain.vectorstores import Pinecone
 import pinecone
 
@@ -14,7 +16,7 @@ pinecone.init(
 INDEX_NAME = "langchain-doc-index"
 
 
-def run_llm(query: str):
+def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
     embeddings = OpenAIEmbeddings(openai_api_key=os.environ["OPENAI_API_KEY"])
     docsearch = Pinecone.from_existing_index(
         embedding=embeddings,
@@ -25,10 +27,7 @@ def run_llm(query: str):
         temperature=0,
     )
 
-    qa = RetrievalQA.from_chain_type(
-        llm=chat,
-        chain_type="stuff",
-        retriever=docsearch.as_retriever(),
-        return_source_documents=True,
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=chat, retriever=docsearch.as_retriever(), return_source_documents=True
     )
-    return qa({"query": query})
+    return qa({"question": query, "chat_history": chat_history})
--- main.py
+++ main.py
@@ -1,36 +1,60 @@
-import streamlit as st
+from typing import Set
+
 from backend.core import run_llm
+import streamlit as st
+from streamlit_chat import message
 
 
-def show_messages(text):
-    messages_str = [
-        f"{_['role']}: {_['content']}" for _ in st.session_state["messages"][1:]
-    ]
-    text.text_area("Messages", value=str("\n".join(messages_str)), height=400)
+def create_sources_string(source_urls: Set[str]) -> str:
+    if not source_urls:
+        return ""
+    sources_list = list(source_urls)
+    sources_list.sort()
+    sources_string = "sources:\n"
+    for i, source in enumerate(sources_list):
+        sources_string += f"{i+1}. {source}\n"
+    return sources_string
 
 
-BASE_PROMPT = [{"role": "system", "content": "You are a helpful assistant."}]
+st.header("LangChain🦜🔗 Udemy Course- Helper Bot")
+if (
+    "chat_answers_history" not in st.session_state
+    and "user_prompt_history" not in st.session_state
+    and "chat_history" not in st.session_state
+):
+    st.session_state["chat_answers_history"] = []
+    st.session_state["user_prompt_history"] = []
+    st.session_state["chat_history"] = []
 
-if __name__ == "__main__":
-    if "messages" not in st.session_state:
-        st.session_state["messages"] = BASE_PROMPT
 
-    st.header("LangChain Udemy Course- Helper Bot")
-    text = st.empty()
-    show_messages(text)
+prompt = st.text_input("Prompt", placeholder="Enter your message here...") or st.button(
+    "Submit"
+)
 
-    prompt = st.text_input("Prompt", value="Enter your message here...")
+if prompt:
+    with st.spinner("Generating response..."):
+        generated_response = run_llm(
+            query=prompt, chat_history=st.session_state["chat_history"]
+        )
 
-    if st.button("Send"):
-        with st.spinner("Generating response..."):
-            st.session_state["messages"] += [{"role": "user", "content": prompt}]
-            message_response = run_llm(query=prompt)
-            formatted_response = message_response["result"]
-            st.session_state["messages"] += [
-                {"role": "system", "content": formatted_response}
-            ]
-            show_messages(text)
+        sources = set(
+            [doc.metadata["source"] for doc in generated_response["source_documents"]]
+        )
+        formatted_response = (
+            f"{generated_response['answer']} \n\n {create_sources_string(sources)}"
+        )
 
-    if st.button("Clear"):
-        st.session_state["messages"] = BASE_PROMPT
-        show_messages(text)
+        st.session_state.chat_history.append((prompt, generated_response["answer"]))
+        st.session_state.user_prompt_history.append(prompt)
+        st.session_state.chat_answers_history.append(formatted_response)
+
+if st.session_state["chat_answers_history"]:
+    for generated_response, user_query in zip(
+        st.session_state["chat_answers_history"],
+        st.session_state["user_prompt_history"],
+    ):
+        message(
+            user_query,
+            is_user=True,
+        )
+        message(generated_response)
