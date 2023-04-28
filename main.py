7682943943671f3a9b34dbdacb645f3cdcc2d2f1
Eden Marco <emarco177@gmail.com>
2023-04-28 07:52:54 +0300

    implemented user interface

---
+++ main.py
@@ -0,0 +1,50 @@
+from typing import Set
+
+from backend.core import run_llm
+import streamlit as st
+from streamlit_chat import message
+
+st.header("LangChain Udemy Course- Documentation Helper Bot")
+
+
+prompt = st.text_input("Prompt", placeholder="Enter your prompt here..")
+
+if "user_prompt_history" not in st.session_state:
+    st.session_state["user_prompt_history"] = []
+
+if "chat_answers_history" not in st.session_state:
+    st.session_state["chat_answers_history"] = []
+
+
+def create_sources_string(source_urls: Set[str]) -> str:
+    if not source_urls:
+        return ""
+    sources_list = list(source_urls)
+    sources_list.sort()
+    sources_string = "sources:\n"
+    for i, source in enumerate(sources_list):
+        sources_string += f"{i+1}. {source}\n"
+    return sources_string
+
+
+if prompt:
+    with st.spinner("Generating response.."):
+        generated_response = run_llm(query=prompt)
+        sources = set(
+            [doc.metadata["source"] for doc in generated_response["source_documents"]]
+        )
+
+        formatted_response = (
+            f"{generated_response['result']} \n\n {create_sources_string(sources)}"
+        )
+
+        st.session_state["user_prompt_history"].append(prompt)
+        st.session_state["chat_answers_history"].append(formatted_response)
+
+if st.session_state["chat_answers_history"]:
+    for generated_response, user_query in zip(
+        st.session_state["chat_answers_history"],
+        st.session_state["user_prompt_history"],
+    ):
+        message(user_query, is_user=True)
+        message(generated_response)
