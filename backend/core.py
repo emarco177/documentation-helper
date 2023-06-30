eaeeaed31e1ecb0358b92b131902771aeb8e9593
Maxime Berthelot <maxime@buffer.com>
2023-06-30 10:44:14 +0200

    added backend

--- backend/core.py
+++ backend/core.py
@@ -1,10 +1,10 @@
 import os
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import RetrievalQA
+from langchain.chains import ConversationalRetrievalChain
 from langchain.vectorstores import Pinecone
 import pinecone
-from typing import Any
+from typing import Any, Dict, List
 
 from consts import INDEX_NAME
 
@@ -14,17 +14,16 @@ pinecone.init(
 )
 
 
-def run_llm(query: str) -> Any:
+def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
     embeddings = OpenAIEmbeddings()
     docsearch = Pinecone.from_existing_index(INDEX_NAME, embeddings)
     chat = ChatOpenAI(verbose=True, temperature=0)
-    qa = RetrievalQA.from_chain_type(
-        llm=chat,
-        chain_type="stuff",
-        retriever=docsearch.as_retriever(),
-        return_source_documents=True,
+
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=chat, retriever=docsearch.as_retriever(), return_source_documents=True
     )
-    return qa({"query": query})
+
+    return qa({"question": query, "chat_history": chat_history})
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -14,6 +14,8 @@ if "user_prompt_history" not in st.session_state:
 if "chat_answers_history" not in st.session_state:
     st.session_state["chat_answers_history"] = []
 
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
 
 
 def create_sources_string(source_urls: Set[str]) -> str:
@@ -30,17 +32,18 @@ def create_sources_string(source_urls: Set[str]) -> str:
 
 if prompt:
     with st.spinner("Thinking..."):
-        generated_response = run_llm(query=prompt)
+        generated_response = run_llm(query=prompt, chat_history=st.session_state["chat_history"])
         sources = set(
             [doc.metadata["source"] for doc in generated_response["source_documents"]]
         )
 
         formated_response = (
-            f"{generated_response['result']}\n\n {create_sources_string(sources)}"
+            f"{generated_response['answer']} \n\n {create_sources_string(sources)}"
         )
 
         st.session_state["user_prompt_history"].append(prompt)
         st.session_state["chat_answers_history"].append(formated_response)
+        st.session_state["chat_history"].append((prompt, generated_response["answer"]))
 
 if st.session_state["chat_answers_history"]:
     for generated_response, user_query in zip(st.session_state["chat_answers_history"], st.session_state["user_prompt_history"]):
