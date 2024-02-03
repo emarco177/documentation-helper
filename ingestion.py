58760445aab1316802992689d4d26c2c443ef4cb
Eden Marco <emarco177@gmail.com>
2024-02-03 14:51:59 +0200

    updated ingestion according to langchain latest

--- ingestion.py
+++ ingestion.py
@@ -1,35 +1,42 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
 import os
 
-from langchain.document_loaders import ReadTheDocsLoader
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.document_loaders import ReadTheDocsLoader
+from langchain_openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Pinecone
-import pinecone
+from langchain_community.vectorstores import Pinecone as PineconeLangChain
+from pinecone import Pinecone
 
-pinecone.init(
-    api_key=os.environ["PINECONE_API_KEY"],
-    environment=os.environ["PINECONE_ENVIRONMENT_REGION"],
+
+pc = Pinecone(
+    api_key=os.environ.get("PINECONE_API_KEY"),
 )
+
 INDEX_NAME = "langchain-doc-index"
 
 
 def ingest_docs():
-    loader = ReadTheDocsLoader("python.langchain.com/en/latest/index.html")
+    loader = ReadTheDocsLoader(
+        "langchain-docs/api.python.langchain.com/en/latest/chains"
+    )
+
     raw_documents = loader.load()
     print(f"loaded {len(raw_documents)} documents")
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=400, chunk_overlap=50, separators=["\n\n", "\n", " ", ""]
-    )
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=50)
     documents = text_splitter.split_documents(raw_documents)
     for doc in documents:
         new_url = doc.metadata["source"]
         new_url = new_url.replace("langchain-docs", "https:/")
         doc.metadata.update({"source": new_url})
 
-    embeddings = OpenAIEmbeddings()
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
     print(f"Going to add {len(documents)} to Pinecone")
-    Pinecone.from_documents(documents, embeddings, index_name=INDEX_NAME)
-    print("****Loading to vectorestore done ***")
+    PineconeLangChain.from_documents(documents, embeddings, index_name=INDEX_NAME)
+    print("****Loading to vectorstore done ***")
 
 
 if __name__ == "__main__":
