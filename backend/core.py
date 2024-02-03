2ecc17ffef6428b6c60bfcba0764ff828f98319f
Eden Marco <emarco177@gmail.com>
2024-02-03 15:00:59 +0200

    updated main according to langchain latest version

--- backend/core.py
+++ backend/core.py
@@ -1,24 +1,24 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
 import os
 from typing import Any, Dict, List
-
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
-from langchain.vectorstores import Pinecone
-import pinecone
+from langchain_community.vectorstores.pinecone import Pinecone as PineconeLangChain
+from pinecone import Pinecone
 
 
-pinecone.init(
-    api_key=os.environ["PINECONE_API_KEY"],
-    environment=os.environ["PINECONE_ENVIRONMENT_REGION"],
-)
+pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
 
 INDEX_NAME = "langchain-doc-index"
 
 
 def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
-    embeddings = OpenAIEmbeddings(openai_api_key=os.environ["OPENAI_API_KEY"])
-    docsearch = Pinecone.from_existing_index(
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+    docsearch = PineconeLangChain.from_existing_index(
         embedding=embeddings,
         index_name=INDEX_NAME,
     )
@@ -30,4 +30,4 @@ def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
     qa = ConversationalRetrievalChain.from_llm(
         llm=chat, retriever=docsearch.as_retriever(), return_source_documents=True
     )
-    return qa({"question": query, "chat_history": chat_history})
+    return qa.invoke({"question": query, "chat_history": chat_history})
