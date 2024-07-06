4c7d5f7b5609bc6dc6f30de8f4552b0e45787efc
Eden Marco <emarco177@gmail.com>
2024-07-06 16:52:38 +0300

    updated to langchain latest 0.2.6

---
+++ backend/core.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+from langchain.chains.retrieval import create_retrieval_chain
+
+load_dotenv()
+
+from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_pinecone import PineconeVectorStore
+
+
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
+
+INDEX_NAME = "langchain-doc-index"
+
+
+def run_llm(query: str):
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+    docsearch = PineconeVectorStore(index_name=INDEX_NAME, embedding=embeddings)
+    chat = ChatOpenAI(verbose=True, temperature=0)
+
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    stuff_documents_chain = create_stuff_documents_chain(chat, retrieval_qa_chat_prompt)
+
+    qa = create_retrieval_chain(
+        retriever=docsearch.as_retriever(), combine_docs_chain=stuff_documents_chain
+    )
+    result = qa.invoke(input={"input": query})
+    return result
+
+
+if __name__ == "__main__":
+    res = run_llm(query="What is a LangChain Chain?")
+    print(res["answer"])
