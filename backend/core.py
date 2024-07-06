02945dd8769bd708da6d6c61ebf29f99b352623e
Eden Marco <emarco177@gmail.com>
2024-07-06 15:51:12 +0300

    updated to langchain latest 0.2.6

--- backend/core.py
+++ backend/core.py
@@ -1,33 +1,39 @@
 from dotenv import load_dotenv
+from langchain.chains.retrieval import create_retrieval_chain
 
 load_dotenv()
 
+from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_pinecone import PineconeVectorStore
+
+
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
-import os
-from typing import Any, Dict, List
-from langchain.chains import ConversationalRetrievalChain
-from langchain_community.vectorstores.pinecone import Pinecone as PineconeLangChain
-from pinecone import Pinecone
-
-
-pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
 
 INDEX_NAME = "langchain-doc-index"
 
 
-def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
+def run_llm(query: str):
     embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
-    docsearch = PineconeLangChain.from_existing_index(
-        embedding=embeddings,
-        index_name=INDEX_NAME,
-    )
-    chat = ChatOpenAI(
-        verbose=True,
-        temperature=0,
-    )
+    docsearch = PineconeVectorStore(index_name=INDEX_NAME, embedding=embeddings)
+    chat = ChatOpenAI(verbose=True, temperature=0)
 
-    qa = ConversationalRetrievalChain.from_llm(
-        llm=chat, retriever=docsearch.as_retriever(), return_source_documents=True
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    stuff_documents_chain = create_stuff_documents_chain(chat, retrieval_qa_chat_prompt)
+
+    qa = create_retrieval_chain(
+        retriever=docsearch.as_retriever(), combine_docs_chain=stuff_documents_chain
     )
-    return qa.invoke({"question": query, "chat_history": chat_history})
+    result = qa.invoke(input={"input": query})
+    new_result = {
+        "query": result["input"],
+        "result": result["answer"],
+        "source_documents": result["context"],
+    }
+    return new_result
+
+
+if __name__ == "__main__":
+    res = run_llm(query="What is a LangChain Chain?")
+    print(res["result"])
