c927e57f5ee00c073eca3c6134e30172c0e52026
huy.to.ryan <huy.to.ryan@gmail.com>
2025-02-13 17:38:34 +0700

    Change model type

--- backend/core.py
+++ backend/core.py
@@ -11,24 +11,32 @@ from langchain import hub
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain.chains.retrieval import create_retrieval_chain
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+# from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_ollama import ChatOllama
+from langchain_ollama.embeddings import OllamaEmbeddings
+
 from langchain_pinecone import PineconeVectorStore
 
 from consts import INDEX_NAME
 
 
 def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
-    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+    # embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
     docsearch = PineconeVectorStore(index_name=INDEX_NAME, embedding=embeddings)
-    chat = ChatOpenAI(verbose=True, temperature=0)
+    # chat_model = ChatOpenAI(verbose=True, temperature=0)
+    embeddings = OllamaEmbeddings(model="llama3.1")
+    chat_model = ChatOllama(
+        model="llama3.1",
+        temperature=0,
+    )
 
     rephrase_prompt = hub.pull("langchain-ai/chat-langchain-rephrase")
 
     retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
-    stuff_documents_chain = create_stuff_documents_chain(chat, retrieval_qa_chat_prompt)
+    stuff_documents_chain = create_stuff_documents_chain(chat_model, retrieval_qa_chat_prompt)
 
     history_aware_retriever = create_history_aware_retriever(
-        llm=chat, retriever=docsearch.as_retriever(), prompt=rephrase_prompt
+        llm=chat_model, retriever=docsearch.as_retriever(), prompt=rephrase_prompt
     )
     qa = create_retrieval_chain(
         retriever=history_aware_retriever, combine_docs_chain=stuff_documents_chain
@@ -43,11 +51,16 @@ def format_docs(docs):
 
 
 def run_llm2(query: str, chat_history: List[Dict[str, Any]] = []):
-    embeddings = OpenAIEmbeddings()
+    # embeddings = OpenAIEmbeddings()
     docsearch = PineconeVectorStore(index_name=INDEX_NAME, embedding=embeddings)
-    chat = ChatOpenAI(model_name="gpt-4o", verbose=True, temperature=0)
+    # chat_model = ChatOpenAI(model_name="gpt-4o", verbose=True, temperature=0)
+    embeddings = OllamaEmbeddings(model="llama3.1")
+    chat_model = ChatOllama(
+        model="llama3.1",
+        temperature=0,
+    )
 
-    rephrase_prompt = hub.pull("langchain-ai/chat-langchain-rephrase")
+    #rephrase_prompt = hub.pull("langchain-ai/chat-langchain-rephrase")
 
     retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
 
@@ -57,15 +70,17 @@ def run_llm2(query: str, chat_history: List[Dict[str, Any]] = []):
             "input": RunnablePassthrough(),
         }
         | retrieval_qa_chat_prompt
-        | chat
+        | chat_model
         | StrOutputParser()
     )
 
     retrieve_docs_chain = (lambda x: x["input"]) | docsearch.as_retriever()
 
-    chain = RunnablePassthrough.assign(context=retrieve_docs_chain).assign(
+    qa = RunnablePassthrough.assign(
+        context=retrieve_docs_chain
+    ).assign(
         answer=rag_chain
     )
 
-    result = chain.invoke({"input": query, "chat_history": chat_history})
+    result = qa.invoke({"input": query, "chat_history": chat_history})
     return result
